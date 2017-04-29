32767b858fa5490f0b363cd6ee974e50a4b5acc1
Srinivas.Narula <srinivas.narula@mnscorp.net>
2017-04-29 15:08:11 +0100

    Default template code

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+(() => {
+  // The initialize function must be run each time a new page is loaded
+  Office.initialize = (reason) => {
+    $(document).ready(() => {
+      $('#run').click(run);
+    });
+  };
+
+  async function run() {
+    
+    
+    /**
+     * Insert your Outlook code here
+     */
+    
+  }
+})();
